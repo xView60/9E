fa725440ca5179d7c32951fd1bdd95cf842abaa6
xView <edymaftei12@gmail.com>
2020-03-12 23:19:54 +0200

    Adaugat Prefix

--- index.js
+++ index.js
@@ -3,12 +3,36 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity(`Peci e prost`)
 });
+const config = require("./config.json");
+client.on('message', message => {
+   // This event will run on every single message received, from any channel or DM.
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  // It's good practice to ignore other bots. This also makes your bot ignore itself
+  // and not get into a spam loop (we call that "botception").
+  if (message.author.bot) return;
+
+  // Also good practice to ignore any message that does not start with our prefix,
+  // which is set in the configuration file.
+  if (message.content.indexOf(config.prefix) !== 0) return;
+
+  // Here we separate our "command" name, and our "arguments" for the command.
+  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
+  // command = say
+  // args = ["Is", "this", "the", "real", "life?"]
+  const args = message.content
+    .slice(config.prefix.length)
+    .trim()
+    .split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  // Let's go with a few common example commands! Feel free to delete or change those.
+  if (command === "test")
+  {
+    message.channel.send("test")
   }
+ 
 });
 
-client.login('NDY1OTMyMTk5NDc3Mzc5MDg0.Xmo9iw.jVVoTXzrAYaYcAqgSTAVJvz1zjo');
+client.login(config.token);
